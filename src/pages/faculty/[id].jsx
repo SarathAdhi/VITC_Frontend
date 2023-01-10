c9773568e708401382343fc87e56618224053732
SarathAdhi <s.sarathadhithya@gmail.com>
2023-01-10 18:13:06 +0530

    minor changes

--- src/pages/faculty/[id].jsx
+++ src/pages/faculty/[id].jsx
@@ -1,12 +1,11 @@
 import { LinkedItem } from "@components/LinkedItem";
-import { H3 } from "@components/Text";
+import { SlideOver } from "@elements/SlideOver";
 import { MenuIcon } from "@heroicons/react/outline";
 import { PageWrapper } from "@layouts/PageWrapper";
 import axios from "@lib/axios";
 import { ViewFacultyDetails } from "@modules/faculty/View";
 import { getIdFormat } from "@utils/format";
 import clsx from "clsx";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
@@ -40,14 +39,14 @@ const sidebarLinks = [
   { dbName: "", name: "Other Information" },
 ];
 
-const LeftSideBar = ({ name, className, image, sidebarNavLinks }) => {
+const LeftSideBar = ({ className, image, sidebarNavLinks }) => {
   const [currentTabKey, setCurrentTabKey] = useState(sidebarNavLinks[0].key);
   const [isMobileNavbarOpen, setIsMobileNavbarOpen] = useState(false);
 
   const NavbarContent = () => (
     <>
-      <div className="relative hidden md:flex w-28 h-40 rounded-[50%] border-[#4b6fa5] border-4">
-        <Image src={image} layout="fill" className="rounded-[50%]" />
+      <div className="hidden md:flex w-44 h-44 rounded-full border-[#4b6fa5] border-4">
+        <img src={image} className="w-full h-full rounded-full" />
       </div>
 
       <div className="flex flex-col">
@@ -57,7 +56,7 @@ const LeftSideBar = ({ name, className, image, sidebarNavLinks }) => {
             href={`#${key}`}
             className={clsx(
               currentTabKey === key && "!text-white",
-              "md:font-bold md:text-lg text-[#ffffff8c] duration-200 hover:text-[#ffffffbf]"
+              "md:font-bold md:text-lg text-[#ffffff8c] duration-200 hover:text-[#ffffffbf] p-2 md:p-0"
             )}
             onClick={() => setCurrentTabKey(key)}
           >
@@ -73,7 +72,7 @@ const LeftSideBar = ({ name, className, image, sidebarNavLinks }) => {
       <div
         className={clsx(
           className,
-          "z-50 sticky top-0 h-screen bg-[#004c93] hidden md:grid gap-5 justify-items-center place-content-center"
+          "z-50 sticky top-0 h-screen bg-[#004c93] hidden md:grid gap-10 justify-items-center place-content-center"
         )}
       >
         <NavbarContent />
@@ -82,25 +81,26 @@ const LeftSideBar = ({ name, className, image, sidebarNavLinks }) => {
       <div
         className={clsx(
           className,
-          "z-50 sticky top-0 p-2 grid md:hidden gap-2 bg-[#004c93]"
+          "z-50 sticky top-0 grid md:hidden gap-2 bg-[#004c93]"
         )}
       >
         <div className="flex items-center justify-between">
-          <H3 className="text-white !font-medium">{name}</H3>
+          <img src="/assets/vitlogo.png" className="h-14" />
 
           <button
             onClick={() => setIsMobileNavbarOpen((pre) => !pre)}
-            className="border border-[#ffffff8c] py-1 px-2 rounded"
+            className="mx-2 border border-[#ffffff8c] py-1 px-2 rounded"
           >
             <MenuIcon className="w-6 h-6 text-[#ffffff8c]" />
           </button>
         </div>
 
-        {isMobileNavbarOpen && (
-          <div className="grid md:hidden gap-5 fixed bg-[#004c93] w-full top-12 p-2 left-0">
-            <NavbarContent />
-          </div>
-        )}
+        <SlideOver
+          isOpen={isMobileNavbarOpen}
+          setIsOpen={setIsMobileNavbarOpen}
+        >
+          <NavbarContent />
+        </SlideOver>
       </div>
     </>
   );
@@ -129,6 +129,8 @@ const ViewStaff = () => {
 
   const facultyDbKeys = Object.entries(facultyDetails);
 
+  console.log({ facultyDetails });
+
   const sidebarNavLinks = sidebarLinks
     .map(({ dbName, name }) => {
       const key = name.toLowerCase().replaceAll(" ", "-");
@@ -148,9 +150,9 @@ const ViewStaff = () => {
     .filter((link) => link !== undefined);
 
   return (
-    <PageWrapper title="" className="w-full grid md:grid-cols-16">
+    <PageWrapper title="" className="w-full flex flex-col md:flex-row">
       <LeftSideBar
-        className="w-full md:col-span-6 lg:col-span-5 xl:col-span-4 2xl:col-span-2"
+        className="w-full md:w-[300px] flex-shrink-0"
         {...facultyDetails}
         router={router}
         sidebarNavLinks={sidebarNavLinks}
